e39cb623a58a14ee920674f3153da5daf1939ff0
TymekBrunka <tymkopolo@wp.pl>
2025-06-30 19:37:01 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,172 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+// use egui_miniquad as egui_mq;
+use miniquad as mq;
+use miniquad::*;
+
+#[repr(C)]
+struct Vertex {
+    pos: [f32; 2],
+    color: [f32; 4],
+}
+
+struct Stage {
+}
+
+struct MyMiniquadApp {
+    pipeline: Pipeline,
+    bindings: Bindings,
+    ctx: Box<dyn RenderingBackend>,
+
+    egui_mq: egui_miniquad::EguiMq,
+}
+
+impl MyMiniquadApp {
+    fn new() -> Self {
+        let mut ctx: Box<dyn RenderingBackend> = window::new_rendering_backend();
+
+        #[rustfmt::skip]
+        let vertices: [Vertex; 3] = [
+            Vertex { pos : [ -0.5, -0.5 ], color: [1., 0., 0., 1.] },
+            Vertex { pos : [  0.5, -0.5 ], color: [0., 1., 0., 1.] },
+            Vertex { pos : [  0.0,  0.5 ], color: [0., 0., 1., 1.] },
+        ];
+        let vertex_buffer = ctx.new_buffer(
+            BufferType::VertexBuffer,
+            BufferUsage::Immutable,
+            BufferSource::slice(&vertices),
+        );
+
+        let indices: [u16; 3] = [0, 1, 2];
+        let index_buffer = ctx.new_buffer(
+            BufferType::IndexBuffer,
+            BufferUsage::Immutable,
+            BufferSource::slice(&indices),
+        );
+
+        let bindings = Bindings {
+            vertex_buffers: vec![vertex_buffer],
+            index_buffer: index_buffer,
+            images: vec![],
+        };
+
+        let shader = ctx
+            .new_shader(
+                ShaderSource::Glsl {
+                    vertex: shader::VERTEX,
+                    fragment: shader::FRAGMENT,
+                },
+                shader::meta(),
+            )
+            .unwrap();
+
+        let pipeline = ctx.new_pipeline(
+            &[BufferLayout::default()],
+            &[
+                VertexAttribute::new("in_pos", VertexFormat::Float2),
+                VertexAttribute::new("in_color", VertexFormat::Float4),
+            ],
+            shader,
+            PipelineParams::default(),
+        );
+
+        MyMiniquadApp {
+            pipeline,
+            bindings,
+
+            egui_mq: egui_miniquad::EguiMq::new(&mut *ctx),
+            ctx,
+        }
+    }
+}
+
+mod shader {
+    use miniquad::*;
+
+    pub const VERTEX: &str = r#"#version 100
+    attribute vec2 in_pos;
+    attribute vec4 in_color;
+
+    varying lowp vec4 color;
+
+    void main() {
+        gl_Position = vec4(in_pos, 0, 1);
+        color = in_color;
+    }"#;
+
+    pub const FRAGMENT: &str = r#"#version 100
+    varying lowp vec4 color;
+
+    void main() {
+        gl_FragColor = color;
+    }"#;
+
+    pub fn meta() -> ShaderMeta {
+        ShaderMeta {
+            images: vec![],
+            uniforms: UniformBlockLayout { uniforms: vec![] },
+        }
+    }
+}
+
+impl mq::EventHandler for MyMiniquadApp {
+    fn update(&mut self) {}
+
+    fn draw(&mut self) {
+        self.ctx .begin_default_pass(mq::PassAction::clear_color(0.0, 0.0, 0.0, 1.0));
+        //gl
+        self.ctx.apply_pipeline(&self.pipeline);
+        self.ctx.apply_bindings(&self.bindings);
+        self.ctx.draw(0, 3, 1);
+        //end gl
+        self.ctx.end_render_pass();
+
+        self.egui_mq.run(&mut *self.ctx, |_mq_ctx, egui_ctx| {
+            // egui::Window::new("Egui Window").show(egui_ctx, |ui| {
+            //     ui.heading("Hello World!");
+            // });
+            egui::SidePanel::right("my_right_panel").show(egui_ctx, |ui| {
+                ui.heading("Hello world");
+            });
+        });
+
+        // Draw things behind egui here
+
+        self.egui_mq.draw(&mut *self.ctx);
+
+        // Draw things in front of egui here
+
+        self.ctx.commit_frame();
+    }
+
+    fn mouse_motion_event(&mut self, x: f32, y: f32) {
+        self.egui_mq.mouse_motion_event(x, y);
+    }
+
+    fn mouse_wheel_event(&mut self, dx: f32, dy: f32) {
+        self.egui_mq.mouse_wheel_event(dx, dy);
+    }
+
+    fn mouse_button_down_event(&mut self, mb: mq::MouseButton, x: f32, y: f32) {
+        self.egui_mq.mouse_button_down_event(mb, x, y);
+    }
+
+    fn mouse_button_up_event(&mut self, mb: mq::MouseButton, x: f32, y: f32) {
+        self.egui_mq.mouse_button_up_event(mb, x, y);
+    }
+
+    fn char_event(&mut self, character: char, _keymods: mq::KeyMods, _repeat: bool) {
+        self.egui_mq.char_event(character);
+    }
+
+    fn key_down_event(&mut self, keycode: mq::KeyCode, keymods: mq::KeyMods, _repeat: bool) {
+        self.egui_mq.key_down_event(keycode, keymods);
+    }
+
+    fn key_up_event(&mut self, keycode: mq::KeyCode, keymods: mq::KeyMods) {
+        self.egui_mq.key_up_event(keycode, keymods);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    miniquad::start(mq::conf::Conf::default(), || Box::new(MyMiniquadApp::new()));
 }
