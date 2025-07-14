4e92d6d1109dee5966383ebbd46130e4d19e5764
TymekBrunka <tymkopolo@wp.pl>
2025-07-14 14:58:37 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ use glam::{Mat4, Vec2, Vec3, vec2, vec3};
 
 use std::time::Instant;
 
-mod cube;
-use cube::*;
+mod util;
+use util::*;
 
 #[repr(C)]
 #[derive(Debug)]
 struct Vertex {
     pos: [f32; 3],
+    typ: f32,
     rat: f32
 }
 
@@ -38,6 +39,8 @@ struct MyMiniquadApp {
     last_frame_t: Instant,
     holding_mouse: bool,
     kb_motion: Vec2,
+
+    screen_size: Vec2,
 }
 
 impl MyMiniquadApp {
@@ -45,6 +48,7 @@ impl MyMiniquadApp {
         let mut ctx: Box<dyn RenderingBackend> = window::new_rendering_backend();
 
         let (width, height) = window::screen_size();
+        let screen_size = vec2(width, height);
         let projection =
             Mat4::perspective_rh_gl(120.0f32.to_radians(), width / height, 0.001, 100000.0);
         let camera: Camera = Camera {
@@ -65,11 +69,13 @@ impl MyMiniquadApp {
         //
 
         gen_cube!(v, i, ic, vc, -20., -6., -30., 40., 6., 60.);
+        gen_point!(v, i, ic, vc, 0., 0., 0.);
 
         //
         //
 
         // println!("{:#?}", v);
+        println!("{:b}", 0x00000001 as u32);
 
         #[rustfmt::skip]
         // let vertices: &[Vertex] = &[
@@ -123,6 +129,7 @@ impl MyMiniquadApp {
             ],
             &[
                 VertexAttribute::new("in_pos", VertexFormat::Float3),
+                VertexAttribute::new("typ", VertexFormat::Float1),
                 VertexAttribute::new("rat", VertexFormat::Float1),
             ],
             shader,
@@ -130,6 +137,12 @@ impl MyMiniquadApp {
                 cull_face: CullFace::Back,
                 depth_test: Comparison::LessOrEqual,
                 depth_write: true,
+
+                color_blend: Some(BlendState::new(
+                    Equation::Add,
+                    BlendFactor::Value(BlendValue::SourceAlpha),
+                    BlendFactor::OneMinusValue(BlendValue::SourceAlpha))
+                ),
                 ..Default::default()
             },
         );
@@ -146,7 +159,9 @@ impl MyMiniquadApp {
             prev_pos: vec2(0., 0.),
             last_frame_t: Instant::now(),
             holding_mouse: false,
-            kb_motion: vec2(0.0, 0.0)
+            kb_motion: vec2(0.0, 0.0),
+
+            screen_size
         }
     }
 }
@@ -167,34 +182,70 @@ mod shader {
 
     pub const VERTEX: &str = r#"#version 100
     attribute vec3 in_pos;
+    attribute float typ;
     attribute float rat;
 
     varying lowp vec4 color;
+    varying mediump float typp;
+    varying mediump float ratt;
+    varying lowp vec2 point_cord;
 
-    uniform mat4 mpv;
+    uniform mediump mat4 mpv;
+    uniform mediump vec2 unit_size;
 
     void main()
     {
-        gl_Position = mpv * vec4(in_pos * vec3(1.0, 1.0, -1.0), 1.0);;
-        color = mix(
-            vec4(0.1, 0.0, 0.05, 1.0),
-            mix(vec4(0.3, 0.6, 0.5, 1.0), vec4(0.2, 0.4, 0.6, 1.0), rat),
-            1.0 / clamp(gl_Position.z, 10.0, 10000.0) * 10.0
-        );
+        typp = typ;
+        gl_Position = mpv * vec4(in_pos * vec3(1.0, 1.0, -1.0), 1.0);
+        if (typ == 1.0) {
+            color = mix(
+                vec4(0.1, 0.0, 0.05, 1.0),
+                mix(vec4(0.3, 0.6, 0.5, 1.0), vec4(0.2, 0.4, 0.6, 1.0), rat),
+                1.0 / clamp(gl_Position.z, 10.0, 10000.0) * 10.0
+                // typ
+            );
+        } else if (typ == 2.0) {
+            gl_Position.xyz /= gl_Position.w;
+            gl_Position.z = -clamp(gl_Position.z, -1.0, 5.0);
+            // gl_Position.xyz /= gl_Position.w;
+            gl_Position.w = 1.0;
+            if (rat == 0.0) {
+                gl_Position.xy += vec2(-0.01, 0.02);
+                point_cord = vec2(-1.0, 2.0);
+            } else if (rat == 1.0) {
+                gl_Position.xy += vec2(-0.01, -0.01);
+                point_cord = vec2(-1.0, -1.0);
+            } else {
+                gl_Position.xy += vec2(0.02, -0.01);
+                point_cord = vec2(2.0, -1.0);
+            }
+        }
     }"#;
 
     pub const FRAGMENT: &str = r#"#version 100
     varying lowp vec4 color;
+    varying mediump float typp;
+    varying mediump float ratt;
+    varying lowp vec2 point_cord;
+
+    lowp float a;
+
+    uniform mediump mat4 mpv;
 
     void main() {
         gl_FragColor = color;
+        if (typp == 2.0) {
+            a = 1.0 - length(point_cord);
+            gl_FragColor = mix(vec4(0.0), vec4(1.0), a * 2.0);
+            // gl_FragColor = vec4(1.0, 0.0, 0.0, 0.0);
+        }
     }"#;
 
     pub fn meta() -> ShaderMeta {
         ShaderMeta {
             images: vec![],
             uniforms: UniformBlockLayout {
-                uniforms: vec![UniformDesc::new("mpv", UniformType::Mat4)],
+                uniforms: vec![UniformDesc::new("mpv", UniformType::Mat4), UniformDesc::new("unit_size", UniformType::Float2)],
             },
         }
     }
@@ -202,6 +253,7 @@ mod shader {
     #[repr(C)]
     pub struct Uniforms {
         pub mpv: glam::Mat4,
+        pub unit_size: glam::Vec2
     }
 }
 
@@ -227,10 +279,10 @@ impl mq::EventHandler for MyMiniquadApp {
 
         let view = Mat4::look_at_rh(self.camera.position, self.camera.position + forward, up);
         let vs_params = shader::Uniforms {
-            mpv: self.camera.projection
-                * view
+            mpv: self.camera.projection * view,
                 // * (Mat4::from_rotation_y(self.camera.orientation.x)
                 //     * Mat4::from_rotation_x(self.camera.orientation.y)),
+            unit_size: vec2(1., 1.) / self.screen_size
         };
 
         self.ctx
--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,15 @@
 macro_rules! gen_cube {
     ($vertex_vec:tt, $indiecies_vec:tt, $indieciec_count:tt, $verticies_count:tt, $x:expr, $y:expr, $z:expr, $width:expr, $height:expr, $length:expr) => {
         $vertex_vec.reserve(36);
-        $vertex_vec.push( Vertex { pos : [ $x, $y + $height, $z ], rat: 1. });
-        $vertex_vec.push( Vertex { pos : [ $x, $y, $z ], rat: 0. });
-        $vertex_vec.push( Vertex { pos : [ $x + $width, $y, $z ], rat: 0. });
-        $vertex_vec.push( Vertex { pos : [ $x + $width, $y + $height, $z ], rat: 1. });
+        $vertex_vec.push( Vertex { pos : [ $x, $y + $height, $z ], typ: 1.0, rat: 1. });
+        $vertex_vec.push( Vertex { pos : [ $x, $y, $z ], typ: 1.0, rat: 0. });
+        $vertex_vec.push( Vertex { pos : [ $x + $width, $y, $z ], typ: 1.0, rat: 0. });
+        $vertex_vec.push( Vertex { pos : [ $x + $width, $y + $height, $z ], typ: 1.0, rat: 1. });
 
-        $vertex_vec.push( Vertex { pos : [ $x, $y + $height, $z + $length ], rat: 1. });
-        $vertex_vec.push( Vertex { pos : [ $x, $y, $z + $length ], rat: 0. });
-        $vertex_vec.push( Vertex { pos : [ $x + $width, $y, $z + $length ], rat: 0. });
-        $vertex_vec.push( Vertex { pos : [ $x + $width, $y + $height, $z + $length ], rat: 1. });
+        $vertex_vec.push( Vertex { pos : [ $x, $y + $height, $z + $length ], typ: 1.0, rat: 1. });
+        $vertex_vec.push( Vertex { pos : [ $x, $y, $z + $length ], typ: 1.0, rat: 0. });
+        $vertex_vec.push( Vertex { pos : [ $x + $width, $y, $z + $length ], typ: 1.0, rat: 0. });
+        $vertex_vec.push( Vertex { pos : [ $x + $width, $y + $height, $z + $length ], typ: 1.0, rat: 1. });
 
         $indiecies_vec.push($verticies_count);
         $indiecies_vec.push($verticies_count + 1);
@@ -58,4 +58,20 @@ macro_rules! gen_cube {
     };
 }
 
-pub(crate) use gen_cube;
+macro_rules! gen_point {
+    ($vertex_vec:tt, $indiecies_vec:tt, $indieciec_count:tt, $verticies_count:tt, $x:expr, $y:expr, $z:expr) => {
+        $vertex_vec.reserve(3);
+        $vertex_vec.push( Vertex { pos : [ $x, $y, $z ], typ: 2.0, rat: 0. });
+        $vertex_vec.push( Vertex { pos : [ $x, $y, $z ], typ: 2.0, rat: 1. });
+        $vertex_vec.push( Vertex { pos : [ $x, $y, $z ], typ: 2.0, rat: 2. });
+
+        $indiecies_vec.push($verticies_count);
+        $indiecies_vec.push($verticies_count + 1);
+        $indiecies_vec.push($verticies_count + 2);
+
+        $verticies_count += 3;
+        $indieciec_count += 3;
+    }
+}
+
+pub(crate) use {gen_cube, gen_point};
