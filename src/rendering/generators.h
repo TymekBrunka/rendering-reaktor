91203996a9718107ae041ca7093f3e597717f946
TymekBrunka <tymkopolo@wp.pl>
2025-10-03 15:00:38 +0200

    crime

--- src/rendering/generators.h
+++ src/rendering/generators.h
@@ -1,6 +1,9 @@
 #ifndef __RR_R_R_GENERATORS
 #define __RR_R_R_GENERATORS
 
-#define RR_AUTOATTRIB(name, __VA_ARGS__) \
-    _Generic()
+#define RR_AUTOATTRIB(Stuct, Name, Location) \
+    Location = glGetUniformLocation(program, ##Name);\
+    glEnableVertexAttribArray(Location); \
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, \
+                          sizeof(Struct), (void*) offsetof(Struct, Name));
 #endif
